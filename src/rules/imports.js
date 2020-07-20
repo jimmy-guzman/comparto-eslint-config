26a2253b573a7248f4f968518bc2f924ca8ce3a0
jimmy-guzman <jimmy.guzman@outlook.com>
2020-07-19 21:20:20 -0500

    feat(imports): 🎸 added more rules
    BREAKING CHANGE: 🧨 some of these new import rules are not auto-fixable(--fix)
    
    ✅ Closes: #38

--- src/rules/imports.js
+++ src/rules/imports.js
@@ -4,7 +4,9 @@ module.exports = {
   plugins: ['import'],
   rules: {
     'import/default': 'error',
+    'import/dynamic-import-chunkname': 'off',
     'import/export': 'error',
+    'import/exports-last': 'off',
     'import/extensions': [
       'error',
       'ignorePackages',
@@ -16,38 +18,58 @@ module.exports = {
         tsx: 'never'
       }
     ],
-
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/first.md
     'import/first': 'error',
+    'import/group-exports': 'off',
+    'import/max-dependencies': 'off',
     'import/named': 'error',
     'import/namespace': 'error',
-
-    // https://github.com/benmosher/eslint-plugin-import/blob/master/docs/rules/no-absolute-path.md
+    'import/newline-after-import': 'error',
     'import/no-absolute-path': [
       'error',
       { esmodule: true, commonjs: true, amd: false }
     ],
+    'import/no-amd': 'error',
+    'import/no-anonymous-default-export': 'off',
+    'import/no-commonjs': 'off',
+    'import/no-cycle': 'warn',
+    'import/no-default-export': 'off',
+    'import/no-deprecated': 'warn',
     'import/no-duplicates': 'error',
+    'import/no-dynamic-require': 'off',
+    'import/no-extraneous-dependencies': 'error',
+    'import/no-internal-modules': 'off',
+    'import/no-mutable-exports': 'error',
     'import/no-named-as-default': 'warn',
     'import/no-named-as-default-member': 'warn',
+    'import/no-named-default': 'error',
+    'import/no-named-export': 'off',
+    'import/no-namespace': 'off',
+    'import/no-nodejs-modules': 'off',
+    'import/no-relative-parent-imports': 'off',
+    'import/no-restricted-paths': 'off',
+    'import/no-self-import': 'error',
+    'import/no-unassigned-import': 'off',
     'import/no-unresolved': ['error', { commonjs: true, caseSensitive: true }],
+    'import/no-unused-modules': 'off',
+    'import/no-useless-path-segments': 'off',
+    'import/no-webpack-loader-syntax': 'error',
     'import/order': [
       'error',
       {
         'groups': [['builtin', 'external', 'internal']],
         'newlines-between': 'always'
       }
-    ]
+    ],
+    'import/prefer-default-export': 'off',
+    'import/unambiguous': 'off'
   },
   settings: {
     'import/extensions': ['.js', '.mjs', '.jsx', '.ts', '.tsx'],
     'import/ignore': ['node_modules'],
     'import/resolver': {
+      node: {},
       webpack: {
         config: doesFileExist('webpack.config.js') ? 'webpack.config.js' : {}
-      },
-      node: {
-        extensions: ['.js', '.jsx', '.ts', '.tsx', '.json']
       }
     }
   }
