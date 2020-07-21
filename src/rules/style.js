d37e39793484e46c2559ab51c0a1b1bfe7ff3c5b
jimmy-guzman <jimmy.guzman@outlook.com>
2020-07-20 22:01:05 -0500

    feat(style): 🎸 introduced more rules, eslint@7.5.0 support
    BREAKING CHANGE: 🧨 requires eslint@7.5.0, most new rules are not auto-fixable(--fix)

--- src/rules/style.js
+++ src/rules/style.js
@@ -7,7 +7,21 @@ module.exports = {
   ],
   plugins: ['prettier', 'jest'],
   rules: {
-    // https://eslint.org/docs/rules/lines-around-comment
+    'camelcase': 'off', // revisit
+    'capitalized-comments': 'off',
+    'consistent-this': 'off',
+    'func-name-matching': 'error',
+    'func-names': 'error',
+    'func-style': 'off',
+    'id-denylist': ['error', 'data', 'err', 'e', 'cb', 'callback'],
+    'id-length': 'off',
+    'id-match': [
+      // camelCase, PascalCase, __identifier, CONST_VALUE, stream$, $el
+      'error',
+      '^\\$?(__)?(([A-Z]|[a-z]|[0-9]+)|([A-Z_]))*\\$?$'
+    ],
+    'init-declarations': 'off',
+    'line-comment-position': 'off',
     'lines-around-comment': [
       'error',
       {
@@ -19,26 +33,51 @@ module.exports = {
         beforeLineComment: true
       }
     ],
-
-    // https://eslint.org/docs/rules/no-nested-ternary (fixable)
+    'lines-between-class-members': 'off',
+    'max-depth': ['error', 4],
+    'max-lines': 'off',
+    'max-lines-per-function': ['error', 100],
+    'max-nested-callbacks': ['error', 7],
+    'max-params': ['error', 7],
+    'max-statements': ['error', 40],
+    'max-statements-per-line': ['error', { max: 1 }],
+    'multiline-comment-style': 'off',
+    'new-cap': 'error',
+    'no-array-constructor': 'error',
+    'no-bitwise': 'off',
+    'no-continue': 'error',
+    'no-inline-comments': 'off',
+    'no-lonely-if': 'error',
+    'no-multi-assign': 'error',
+    'no-negated-condition': 'error',
     'no-nested-ternary': 'error',
-
-    // https://eslint.org/docs/rules/no-unneeded-ternary (fixable)
+    'no-new-object': 'error',
+    'no-plusplus': 'off',
+    'no-restricted-syntax': 'off',
+    'no-ternary': 'off',
+    'no-underscore-dangle': 'off',
     'no-unneeded-ternary': 'error',
-
-    // https://eslint.org/docs/rules/prefer-object-spread
+    'one-var': 'off',
+    'operator-assignment': 'off',
+    'padding-line-between-statements': [
+      'error',
+      { blankLine: 'always', prev: ['const', 'let', 'var'], next: '*' },
+      {
+        blankLine: 'any',
+        prev: ['const', 'let', 'var'],
+        next: ['const', 'let', 'var']
+      }
+    ],
+    'prefer-exponentiation-operator': 'warn',
     'prefer-object-spread': 'error',
-
     'prettier/prettier': 'error',
-
-    // https://eslint.org/docs/rules/sort-keys
     'sort-keys': [
       'error',
       'asc',
       { caseSensitive: true, natural: false, minKeys: 5 }
     ],
-
-    // https://eslint.org/docs/rules/space-before-blocks
-    'space-before-blocks': 'error'
+    'sort-vars': 'off',
+    'space-before-blocks': 'error',
+    'spaced-comment': 'off'
   }
 }
