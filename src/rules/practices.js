9e6b26729fecc54cf05f17cccee3e12921c36b28
jimmy-guzman <jimmy.guzman@outlook.com>
2020-11-21 20:09:16 -0600

    feat(deps): 🎸 eslint@7.14.0

--- src/rules/practices.js
+++ src/rules/practices.js
@@ -40,6 +40,7 @@ module.exports = {
     'no-new': 'error',
     'no-new-func': 'error',
     'no-new-wrappers': 'error',
+    'no-nonoctal-decimal-escape': 'off',
     'no-octal': 'error',
     'no-octal-escape': 'error',
     'no-param-reassign': 'off',
